c530c28b4e9fc768616ee5cd8687b65b81bf73ab
badhddct <badhddct@yahoo.com>
2023-06-19 23:12:43 -0400

    new file:   .vscode/launch.json 	modified:   app.html 	modified:   code.html 	new file:   img/rewind.png 	modified:   index.html 	modified:   js/site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World")
-}
+// Get the string from the page
+// Controller function
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+  
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+  
+    displayString(revString);
+  }
+  
+  // Reverse the string
+  // Logic function
+  function reverseString(userString) {
+    let revString = [];
+  
+    // Reverse a string using a loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+      revString += userString[index];
+    }
+  
+    return revString;
+  }
+  
+  // Display the reversed string to the user
+  // View function
+  function displayString(revString) {
+    // Write to page
+    document.getElementById("msg").innerHTML = `Your string is reversed: ${revString}`;
+  
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+  }
+  
